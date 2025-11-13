e22ea376b2f1c707121f7765695d6d16731de453
Filipe <filipemzanin@gmail.com>
2025-11-13 19:16:31 -0300

    Revert "feat: backend version 02"
    This reverts commit 5ad794bdeade65daea5202400554132dc09663a9.

--- prisma/migrations/20251108180832_campos_endereco_os/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `ordem_servico` ADD COLUMN `cep` VARCHAR(9) NULL,
-    ADD COLUMN `complemento` VARCHAR(255) NULL,
-    ADD COLUMN `numero` VARCHAR(10) NULL,
-    ADD COLUMN `rua` VARCHAR(255) NULL;
--- src/server.ts
+++
@@ -1,6 +0,0 @@
-import { app } from "./app.js";
-import { ENV } from "./config/env.js";
-
-app.listen(ENV.PORT, () => {
-    console.log(`Server is running on port ${ENV.PORT}`);
-});
